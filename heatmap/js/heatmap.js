2be894364913ded9b74eb3c7db5fb2d5501cab57
syntagmatic <kai.salmon.chang@gmail.com>
2011-12-20 09:33:24 -0800

    broke heatmap into functions

--- heatmap/js/heatmap.js
+++ heatmap/js/heatmap.js
@@ -15,25 +15,54 @@ function heatmap(id, data) {
     });
   });
 
-  self.canvas.onmousemove = position;
-
-  function position(e) {
-    var x = e.offsetX;
-    var y = e.offsetY;
-    var j = Math.floor(x/totsize);
-    var i = Math.floor(y/totsize);
-    var val = self.data[i][j];
-    document.getElementById('x').innerHTML = x;
-    document.getElementById('y').innerHTML = y;
-    document.getElementById('i').innerHTML = i;
-    document.getElementById('j').innerHTML = j;
-    document.getElementById('val').innerHTML = val;
-  };
+  self.canvas.onmousemove = function(e) { showValues(e, self.data) };
 
   return self;
 };
 
+/* utility functions */
+
+// colorize based on value
 function colorize(val) {
-  //return 'rgb(' + Math.round(255*val) + ',0,0)';
   return 'hsla(0,' + (100*val) +'%,50%,' + val + ')';
 };
+
+// invert mapping to get indices
+function indices(totsize, e) {
+  var x = e.offsetX;
+  var y = e.offsetY;
+  var j = Math.floor(x/totsize);
+  var i = Math.floor(y/totsize);
+  return {
+    x: x,
+    y: y,
+    i: i,
+    j: j
+  };
+};
+
+// lookup value with indices
+function lookup(pos, data) {
+  if (_.isArray(data)) {
+    return data[pos.i][pos.j];      // 2d array
+  } else {
+    if (pos.i in data) {
+      if (pos.j in data[row]) {
+        return data[pos.i][pos.j];  // nested object
+      }
+    }
+  }
+  return undefined;                 // couldn't find anything
+};
+
+// display indices and value
+function showValues(e, data) {
+  var pos = indices(totsize, e);
+  var val = lookup(pos, data);
+  document.getElementById('x').innerHTML = pos.x;
+  document.getElementById('y').innerHTML = pos.y;
+  document.getElementById('i').innerHTML = pos.i;
+  document.getElementById('j').innerHTML = pos.j;
+  document.getElementById('val').innerHTML = val;
+};
+
